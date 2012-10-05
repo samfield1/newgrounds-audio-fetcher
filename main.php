4fd460de5d2375e5ba5133ab7d3b7b42774d7112
GingerPaul55 <paulhappyhutchinson@gmail.com>
2012-10-05 22:38:16 +0000

    Added support for filtering by year and total downloads. Also added more information to useage/help output

--- main.php
+++ main.php
@@ -3,25 +3,64 @@
 	## Install :: apt-get install wget id3v2 php5
 	## If you don't want to use wget IE you just wanna do a pure php download then just find and replacae wgetBase with your patch
 
-	// Define the base of the wget command and what a tab is
-	$wgetBase = 'wget -c -q -U "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1" -O ';
+	// Define defaults
+	$wgetBase = 'wget -c -q -U "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1" -O '; // wget base cmd
 	$id3 = 'id3v2'; // Your id3 tagging program
-	define('TAB', "\t");
+	define('TAB', "\t"); // A tab
+	$targetYear = false;	// The year we're going to download or false for all
+	$downloadLimit = false; // Limit the total downloads
 
 	// Kick out credits
 	echo 'Newgrounds Audio Portal Downloader',PHP_EOL,
 		'By GingerPaul55',PHP_EOL;
 
 	// Prepare usage
-	$file = $_SERVER['argv'];
+	$file = current($_SERVER['argv']);
 
 	// Tidy up
 	unset($_SERVER['argv'][0]);
 
-	// No user? - Show usage
-	if ($_SERVER['argc'] < 2) {
-		echo 'Usage: ', $file[0], ' artist-username-1 artist-username-2 ...',PHP_EOL;
-		die(1);
+	// See what options are being set via command line flags
+	$shortopts = "y:l:h";	// Year & Limit
+	$longopts  = array(
+		"year:",	// Year
+		"limit:",	// Limit
+		"help"		// Help/useage
+	);
+	$options = getopt($shortopts, $longopts);
+
+	// Loop though any flags set
+	foreach ($options as $option => $value) {
+		if ($option == 'y' || $option == 'year') {
+			$targetYear = (int)$value; // Update the year
+			if ($targetYear > date('Y') || $targetYear < 1995) {
+				echo 'The Year (', $targetYear, ') cannot be used because must be between 1995 and ', date('Y'), PHP_EOL;
+				die(1);
+			}
+			array_splice($_SERVER['argv'], 0, 2);
+		} elseif($option == 'l' || $option == 'limit') {
+			$downloadLimit = (int)$value; // Update the max downloads
+			if ($downloadLimit < 1) {
+				echo 'The limit (', $value, ') cannot be used because it must be 1 or higher', PHP_EOL;
+				die(1);
+			}
+			array_splice($_SERVER['argv'], 0, 2);
+		} elseif($option == 'h' || $option == 'help') {
+			array_splice($_SERVER['argv'], 0, 1);
+		} else {
+			echo 'Unknown option (',$option,')',PHP_EOL;
+			die(1);
+		}
+	}
+
+	// No users? - Show usage
+	if (count($_SERVER['argv']) === 0) {
+		echo 'Usage: ', $file, ' [options] artist-username-1 artist-username-2 ...', PHP_EOL, PHP_EOL;
+		echo str_pad('Options Name', 20, ' '), str_pad('GNU long option', 20, ' '), 'Meaning',PHP_EOL;
+		echo str_pad(' -h', 20, ' '), str_pad('--help', 20, ' '), 'Display this help message', PHP_EOL;
+		echo str_pad(' -l', 20, ' '), str_pad('--limit', 20, ' '), 'Limit the total number of MP3 downloads', PHP_EOL;
+		echo str_pad(' -y', 20, ' '), str_pad('--year', 20, ' '), 'Limit the downloads to this year', PHP_EOL;
+		die(0);
 	}
 
 	// Load genre ids
@@ -38,6 +77,7 @@
 	// rename
 	$usernames = &$_SERVER['argv'];
 
+	// Count all the files we download
 	$fileCounter = 0;
 
 	// Lets get jiggy
@@ -75,7 +115,7 @@
 			$username = $matches[1];
 		}
 
-		echo TAB,'Download files for ',$username,PHP_EOL;
+		echo TAB,'Downloading files for ',$username,PHP_EOL;
 
 		// Check for no submissions
 		if (strpos($data, 'does not have any audio submission') !== false) {
@@ -116,7 +156,7 @@
 		// Find the first year
 		preg_match('/<h2 class="audio">(\d{4}) Submissions/', $sections[0], $year);
 		if (isset($year)) {
-			$year = $year[1];
+			$year = (int)$year[1];
 		}
 
 		// Clean up
@@ -124,76 +164,82 @@
 
 		$d = 1; // Disc / Year Counter
 		foreach ($sections as $section) {
+			if ($targetYear !== false && $year !== $targetYear) {
+				echo TAB,TAB, 'Skipping year (', $year, ') because it doesn\'t match our target year (', $targetYear, ')',PHP_EOL;
+			} else {
+				echo TAB,TAB,'Downloading tracks uploaded in ', $year, PHP_EOL;
 
-			echo TAB,TAB,'Downloading tracks uploaded in ',$year,PHP_EOL;
+				// Now look for our links
+				$matches = array();
+				$preg = preg_match_all('/<a href="http:\/\/www.newgrounds.com\/audio\/listen\/(\d*)">([^<]*)<\/a><\/td><td>([^<]*)/', $section, $matches);
 
-			// Now look for our links
-			$matches = array();
-			$preg = preg_match_all('/<a href="http:\/\/www.newgrounds.com\/audio\/listen\/(\d*)">([^<]*)<\/a><\/td><td>([^<]*)/', $section, $matches);
+				$t = count($matches[1]);
+				$files = array();
+				$i = 0;
+				// Foreach audio URL download an mp3
+				while ($i < $t) {
+					// Put in an array so future devs can easily make it threadable
+					$files[$i] = array(
+							'lid' => $i + 1,					// Local ID (Track ID)
+							'ngid' => (int)$matches[1][$i],				// Newgrounds ID
+							'name' => html_entity_decode($matches[2][$i]),		// Song Name
+							'type' => $matches[3][$i],				// Song Type
+							'disc' => $d,						// Disc number
+							'year' => $year,					// Newgrounds Year
+							'file' => $downloadDir.$d.' - '.$i.' - '.str_replace(array('/', '.', '~'), '_',html_entity_decode($matches[2][$i])).'.mp3'	// User - Disc - Track.mp3
+						);
+					echo TAB,TAB,TAB,'Working on Track ', $i + 1, ' of ', $t, ' (',$files[$i]['name'],')...',TAB;
 
-			$t = count($matches[1]);
-			$files = array();
-			$i = 0;
-			// Foreach audio URL download an mp3
-			while ($i < $t) {
-				// Put in an array so future devs can easily make it threadable
-				$files[$i] = array(
-						'lid' => $i + 1,					// Local ID (Track ID)
-						'ngid' => (int)$matches[1][$i],				// Newgrounds ID
-						'name' => html_entity_decode($matches[2][$i]),		// Song Name
-						'type' => $matches[3][$i],				// Song Type
-						'disc' => $d,						// Disc number
-						'year' => $year,					// Newgrounds Year
-						'file' => $downloadDir.$d.' - '.$i.' - '.str_replace(array('/', '.', '~'), '_',html_entity_decode($matches[2][$i])).'.mp3'	// User - Disc - Track.mp3
-					);
-				echo TAB,TAB,TAB,'Working on Track ', $i + 1, ' of ', $t, ' (',$files[$i]['name'],')...',TAB;
+					// Download the file
+					$code = false;
+					$lines= array();
+					$sys = $wgetBase.escapeshellarg($files[$i]['file']).' '.escapeshellarg('http://www.newgrounds.com/audio/download/'.$files[$i]['ngid']);
+					$line = exec($sys, $lines, $code);
 
-				// Download the file
-				$code = false;
-				$lines= array();
-				$sys = $wgetBase.escapeshellarg($files[$i]['file']).' '.escapeshellarg('http://www.newgrounds.com/audio/download/'.$files[$i]['ngid']);
-				$line = exec($sys, $lines, $code);
+					// wget returned a non-zero. An error
+					if ($code !== 0) {
+						echo 'Failed to download file', PHP_EOL;
+						++$i;
+						continue;
+					}
 
-				// wget returned a non-zero. An error
-				if ($code !== 0) {
-					echo 'Failed to download file', PHP_EOL;
+					// Change the Meta data
+					$sys = $id3;
+					$sys.= ' -A '.escapeshellarg('Newgrounds Audio Portal - '.$username);
+					$sys.= ' -t '.escapeshellarg($files[$i]['name']);
+					$sys.= ' -T '.escapeshellarg($files[$i]['lid']);
+					if (strpos($id3,'2') !== false) { // if we're using id3v2
+						$sys.= '/'.$t;
+					}
+					$sys.= ' -a '.escapeshellarg($username);
+					$sys.= ' -y '.escapeshellarg($files[$i]['year']);
+					$sys.= ' -c '.escapeshellarg('Downloaded using GingerPauls NG batch downloader');
+					if (isset($id3GenreCodes[$files[$i]['type']])) {
+						$sys.= ' -g '.escapeshellarg($id3GenreCodes[$files[$i]['type']]);
+					} else {
+						$sys.= ' -g 12';
+					}
+					$sys.= ' '.escapeshellarg($files[$i]['file']);
+					$code = false;
+					$lines = array();
+					$data = exec($sys, $lines, $code);
+
+					echo 'Completed!',PHP_EOL;
+
+					// Next URL/file
 					++$i;
-					continue;
+					++$fileCounter;
+
+					if ($downloadLimit === $fileCounter) {
+						echo 'Download limit (',$downloadLimit,') reached!', PHP_EOL;
+						break 3;
+					}
 				}
-
-				// Change the Meta data
-				$sys = $id3;
-				$sys.= ' -A '.escapeshellarg('Newgrounds Audio Portal - '.$username);
-				$sys.= ' -t '.escapeshellarg($files[$i]['name']);
-				$sys.= ' -T '.escapeshellarg($files[$i]['lid']);
-				if (strpos($id3,'2') !== false) { // if we're using id3v2
-					$sys.= '/'.$t;
-				}
-				$sys.= ' -a '.escapeshellarg($username);
-				$sys.= ' -y '.escapeshellarg($files[$i]['year']);
-				$sys.= ' -c '.escapeshellarg('Downloaded using GingerPauls NG batch downloader');
-				if (isset($id3GenreCodes[$files[$i]['type']])) {
-					$sys.= ' -g '.escapeshellarg($id3GenreCodes[$files[$i]['type']]);
-				} else {
-					$sys.= ' -g 12';
-				}
-				$sys.= ' '.escapeshellarg($files[$i]['file']);
-				$code = false;
-				$lines = array();
-				$data = exec($sys, $lines, $code);
-
-				echo 'Completed!',PHP_EOL;
-
-				// Next URL/file
-				++$i;
-
-				++$fileCounter;
 			}
-
 			// Find out what the next year the user uploaded a file in
 			preg_match('/<h2 class="audio">(\d{4}) Submissions/', $section, $year);
 			if (isset($year[1])) {
-				$year = $year[1];
+				$year = (int)$year[1];
 			}
 
 			// Another year = Another disc
@@ -201,7 +247,7 @@
 		}
 
 		// Finished downloading everything for this user.
-		echo TAB,'Completed processing for ',$username,PHP_EOL;
+		echo TAB, 'Completed processing for ', $username, PHP_EOL;
 
 		// Clean up
 		unset($year);
